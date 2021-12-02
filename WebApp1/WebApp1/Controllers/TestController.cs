0bacf32e45d57eaca95a7f83fb9e7bb6a476c9c6
Sergey G <guryev.s@gmail.com>
2021-12-03 01:52:01 +0300

    Initial commit

---
+++ WebApp1/WebApp1/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public abstract class TestControllerBase : ControllerBase
+    {
+        [HttpGet("base")]
+        public IActionResult GetBase(string value)
+        {
+            return Ok($"Success: {value}");
+        }
+    }
+    
+    public class TestController : TestControllerBase
+    {
+        [HttpGet("own")]
+        public IActionResult GetOwn(string value)
+        {
+            return Ok($"Success: {value}");
+        }
+    }
+}
